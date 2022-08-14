61e60c33465dcc8ab6493402269f1d7dac1cf6c4
Joshtri <stuffofyos1516@gmail.com>
2022-08-14 18:57:28 +0800

    malem-malem

---
+++ console.mjs
@@ -0,0 +1,25 @@
+import { Console } from "console";
+import fs from "fs";
+
+const logFile = fs.createWriteStream("application.log");
+
+const log = new Console({
+    stdout: logFile,
+    stderr: logFile
+
+
+});
+
+
+
+const person = {
+    firstname : "Yostry",
+    lastname :"Lenggu",
+
+}
+log.info("Hello World");
+log.error("Hello World");
+
+
+
+log.table(person);
